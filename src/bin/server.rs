d2f7117b631db624219efac373854b56be6d35eb
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-09-06 00:51:16 +0530

    further refactor directory structure to later add multiple binaries

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,28 +1,29 @@
-#![cfg_attr(debug_assertions, allow(unused_must_use, unused_mut, unused_variables))]
+use etron::server::Server;
 
-pub mod broker;
-pub mod client;
-pub mod message;
-pub mod server;
-
-use server::Server;
 use std::net::SocketAddr;
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let addr = std::env::args()
+        .nth(1)
+        .map(|s| s.parse::<SocketAddr>())
+        .unwrap_or(Ok(SocketAddr::from(([0u8; 4], 3000))))?;
+
     let slog_builder = tracing_subscriber::fmt()
+        .with_file(cfg!(debug_assertions))
         .with_env_filter(EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info")));
 
     #[cfg(debug_assertions)]
     let slog_builder = slog_builder
         .with_file(true)
-        .with_env_filter(EnvFilter::new("TRACE"))
+        .with_env_filter(EnvFilter::new("trace"))
         .with_line_number(true);
 
     slog_builder.init();
 
-    let server = Server::new(SocketAddr::from(([0; 4], 3000))).await?;
+    let server = Server::new(addr).await?;
     server.await?;
+
     Ok(())
 }
