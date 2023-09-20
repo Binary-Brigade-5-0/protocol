4f3d5cdfa0b7459704f374cc619a02f7b92b7a7f
Shubhanshu Tomar <57960301+tomshoo@users.noreply.github.com>
2023-09-20 16:46:05 +0530

    feat: add post header data types

--- src/message/model/mod.rs
+++ src/message/model/mod.rs
@@ -1,7 +1,27 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 pub mod client;
 
+#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug)]
+pub struct PostHeader {
+    id: Uuid,
+    posted: DateTime<Utc>,
+    title: Box<str>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[derive(Clone, Debug)]
+pub struct Post {
+    #[serde(flatten)]
+    header: PostHeader,
+    content: Box<str>,
+}
+
 pub mod server {
-    use uuid::Uuid;
+    use super::Uuid;
 
     pub enum Message {
         Connected(Uuid),
